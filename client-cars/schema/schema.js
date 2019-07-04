90484f0a6f5f1195610a7bf2373e9342e5a97431
arthur <arthur_sbd@hotmail.com>
2019-07-04 18:43:17 -0300

    feat(graphql-upgrade): Adjust in brand

--- client-cars/schema/schema.js
+++ client-cars/schema/schema.js
@@ -9,15 +9,30 @@ const { GraphQLObjectType,
  } = graphql;
 
 const car = require('../models/car');
+const brand = require('../models/brand')
 
 const ModelType = new GraphQLObjectType({
   name: 'Model',
   fields: () => ({
     id: { type: GraphQLID },
     model: { type: GraphQLString },
-    year: { type: GraphQLInt }
+    year: { type: GraphQLInt },
+    brand: {
+      type: BrandType,
+      resolve(parent, args){
+        return _.find(brand, { id: parent.brandId });
+      }
+    }
   })
 });
+
+const BrandType = new GraphQLObjectType({
+  name: 'Brand',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString }
+  })
+})
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -27,6 +42,13 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args){
         return _.find(car, { id: args.id });
+      }
+    },
+    brand: {
+      type: BrandType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent,args){
+        return _.find(brand, { id: args.id })
       }
     }
   })
